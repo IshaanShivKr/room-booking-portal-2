b9cb9db73f5148cbd199a201d87637d88aa2d4a4
Ishaan Shiv Kumar <ishaanshivkr@gmail.com>
2026-01-19 14:47:40 +0530

    Fixed typo

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,8 +1,12 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once __DIR__ . '/../include/autoload.php';
 
-use APP\Database\Database;
+use App\Database\Database;
 
 $config = require __DIR__ . '/../config/database.php';
 
